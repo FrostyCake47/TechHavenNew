0c363c7c29f3c6cb7fe11b6288c20f09a93c725c
Akash <AwesomeAkash47@gmail.com>
2024-04-04 20:41:25 +0530

    next templated

---
+++ app/FanControl/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FanControl = () => {
+  return (
+    <div>FanControl</div>
+  )
+}
+
+export default FanControl
---
+++ app/LightControl/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LightControl = () => {
+  return (
+    <div>LightControl</div>
+  )
+}
+
+export default LightControl
